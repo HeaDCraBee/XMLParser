0df60c9393a672f8cbb7bb32f801a2e9ce6930fa
HeaDCraBee <theheadcrabz@gmail.com>
2020-11-19 15:23:40 +0300

    Add project files.

---
+++ TestTask/Data.cs
@@ -0,0 +1,487 @@
+﻿using System;
+using System.Xml;
+
+
+namespace TestTask
+{
+    /*
+     * Класс OfferType описывает стандартные значения, присутствующее в любом типе offera
+     * Реализует заполнение стандартных полей
+     * Каждый дочерний класс использует методы этого для заполнения стандартных полей и свой метод ParseOther, который заполняет индивидуальные поля этого класса
+     * Можно получить любой компонент offer, написав геттер. Написав сеттер можно их изменять, но нужно написать метод изменения AllData
+     */
+    abstract class OfferType
+    {
+        protected int Id;
+        protected string type;
+        protected int bid;
+        protected bool available;
+        protected string URL;
+        protected double price;
+        protected string currencyID;
+        protected int categoryID;
+        protected string picture;
+        protected string description;
+
+
+
+        public XmlNode AllData { get; set; }
+
+        protected OfferType(XmlNode allData)
+        {
+            AllData = allData;
+
+        }
+
+
+        protected void ParseOfferLine()
+        {
+            Id = Int32.Parse(AllData.Attributes.GetNamedItem("id").Value);
+
+
+            type = AllData.Attributes.GetNamedItem("type").Value;
+
+
+            bid = Int32.Parse(AllData.Attributes.GetNamedItem("bid").Value);
+
+            available = Boolean.Parse(AllData.Attributes.GetNamedItem("available").Value);
+        }
+
+        protected void ParseBase()
+        {
+            foreach (XmlNode childNode in AllData.ChildNodes)
+            {
+                if (childNode.Name == "url")
+                    URL = childNode.InnerText;
+
+                if (childNode.Name == "price")
+                    price = Double.Parse(childNode.InnerText);
+
+                if (childNode.Name == "currencyId")
+                    currencyID = childNode.InnerText;
+
+                if (childNode.Name == "categoryId")
+                    categoryID = Int32.Parse(childNode.InnerText);
+
+                if (childNode.Name == "picture")
+                    picture = childNode.InnerText;
+
+                if (childNode.Name == "description")
+                    description = childNode.InnerText; ;
+            }
+        }
+
+        public int ID
+        {
+            get { return Id; }
+        }
+    }
+
+    class VenderModel : OfferType
+    {
+        private int cbid;
+        private double localDeliveryCost;
+        private string typePrefix;
+        private string vendor;
+        private string vendorCode;
+        private string model;
+        private bool delivery;
+        private bool manufacturerWarranty;
+        private string countryOfOrigin;
+        public VenderModel(XmlNode allData) : base(allData)
+        {
+
+            ParseOfferLine();
+            ParseBase();
+            ParseOther();
+        }
+
+
+        private void ParseOther()
+        {
+            foreach (XmlNode childNode in AllData.ChildNodes)
+            {
+                if (childNode.Name == "cbid")
+                    cbid = Int32.Parse(childNode.InnerText);
+
+                if (childNode.Name == "delivery")
+                    delivery = Boolean.Parse(childNode.InnerText);
+
+                if (childNode.Name == "local_delivery_cost")
+                    localDeliveryCost = Double.Parse(childNode.InnerText);
+
+                if (childNode.Name == "typePrefix")
+                    typePrefix = childNode.InnerText;
+
+                if (childNode.Name == "vendor")
+                    vendor = childNode.InnerText;
+
+                if (childNode.Name == "vendorCode")
+                    vendorCode = childNode.InnerText;
+
+                if (childNode.Name == "model")
+                    model = childNode.InnerText;
+
+                if (childNode.Name == "manufacturer_warranty")
+                    manufacturerWarranty = Boolean.Parse(childNode.InnerText);
+
+                if (childNode.Name == "country_of_origin")
+                    countryOfOrigin = childNode.InnerText;
+            }
+        }
+    }
+
+    class Book : OfferType
+    {
+        private bool delivery;
+        private double localDeliveryCost;
+        private string author;
+        private string name;
+        private string publisher;
+        private string series;
+        private int year;
+        private string ISBN;
+        private int volume;
+        private int part;
+        private string language;
+        private string binding;
+        private int pageExtene;
+        private bool downloadable;
+        public Book(XmlNode allData) : base(allData)
+        {
+            ParseOfferLine();
+            ParseBase();
+            ParseOther();
+        }
+
+        private void ParseOther()
+        {
+            foreach (XmlNode childNode in AllData.ChildNodes)
+            {
+                if (childNode.Name == "delivery")
+                    delivery = Boolean.Parse(childNode.InnerText);
+
+                if (childNode.Name == "local_delivery_cost")
+                    localDeliveryCost = Double.Parse(childNode.InnerText);
+
+                if (childNode.Name == "author")
+                    author = childNode.InnerText;
+
+                if (childNode.Name == "name")
+                    name = childNode.InnerText;
+
+                if (childNode.Name == " publisher")
+                    publisher = childNode.InnerText;
+
+                if (childNode.Name == "series")
+                    series = childNode.InnerText;
+
+                if (childNode.Name == "year")
+                    year = Int32.Parse(childNode.InnerText);
+
+                if (childNode.Name == "ISBN")
+                    ISBN = childNode.InnerText;
+
+                if (childNode.Name == "volume")
+                    volume = Int32.Parse(childNode.InnerText);
+
+                if (childNode.Name == "part")
+                    part = Int32.Parse(childNode.InnerText);
+
+                if (childNode.Name == "language")
+                    language = childNode.InnerText;
+
+                if (childNode.Name == "binding")
+                    binding = childNode.InnerText;
+
+                if (childNode.Name == "page_extent")
+                    pageExtene = Int32.Parse(childNode.InnerText);
+
+                if (childNode.Name == "downloadable")
+                    downloadable = Boolean.Parse(childNode.InnerText);
+
+            }
+        }
+    }
+
+    class AudioBook : OfferType
+    {
+        private string author;
+        private string name;
+        private string publisher;
+        private int year;
+        private string ISBN;
+        private string language;
+        private string performedBy;
+        private string performanceType;
+        private string storage;
+        private string format;
+        private string recordingLength;
+        private bool downloadable;
+
+        public AudioBook(XmlNode allData) : base(allData)
+        {
+            ParseOfferLine();
+            ParseBase();
+            ParseOther();
+        }
+
+        private void ParseOther()
+        {
+
+            foreach (XmlNode childNode in AllData.ChildNodes)
+            {
+                if (childNode.Name == "author")
+                    author = childNode.InnerText;
+
+                if (childNode.Name == "name")
+                    name = childNode.InnerText;
+
+                if (childNode.Name == " publisher")
+                    publisher = childNode.InnerText;
+
+                if (childNode.Name == "year")
+                    year = Int32.Parse(childNode.InnerText);
+
+                if (childNode.Name == "ISBN")
+                    ISBN = childNode.InnerText;
+
+                if (childNode.Name == "language")
+                    language = childNode.InnerText;
+
+                if (childNode.Name == "performed_by")
+                    performedBy = childNode.InnerText;
+
+                if (childNode.Name == "performance_type")
+                    performanceType = childNode.InnerText;
+
+                if (childNode.Name == "storage")
+                    storage = childNode.InnerText;
+
+                if (childNode.Name == "format")
+                    format = childNode.InnerText;
+
+                if (childNode.Name == "recording_length")
+                    recordingLength = childNode.InnerText;
+
+                if (childNode.Name == "downloadable")
+                    downloadable = Boolean.Parse(childNode.InnerText);
+
+            }
+        }
+
+    }
+
+    class ArtistTitle : OfferType
+    {
+        private bool delivery;
+        private string artist;
+        private string title;
+        private int year;
+        private string media;
+        private string starring;
+        private string director;
+        private string originalName;
+        private string country;
+
+        public ArtistTitle(XmlNode allData) : base(allData)
+        {
+            ParseOfferLine();
+            ParseBase();
+            ParseOther();
+        }
+
+        private void ParseOther()
+        {
+            foreach (XmlNode childNode in AllData.ChildNodes)
+            {
+                if (childNode.Name == "delivery")
+                    delivery = Boolean.Parse(childNode.InnerText);
+
+                if (childNode.Name == "artist")
+                    artist = childNode.InnerText;
+
+                if (childNode.Name == "title")
+                    title = childNode.InnerText;
+
+                if (childNode.Name == "year")
+                    year = Int32.Parse(childNode.InnerText);
+
+                if (childNode.Name == " media")
+                    media = childNode.InnerText;
+
+                if (childNode.Name == "starring")
+                    starring = childNode.InnerText;
+
+                if (childNode.Name == "director")
+                    director = childNode.InnerText;
+
+                if (childNode.Name == "originalName")
+                    originalName = childNode.InnerText;
+
+                if (childNode.Name == "country")
+                    country = childNode.InnerText;
+            }
+        }
+    }
+
+    class Tour : OfferType
+    {
+        private bool delivery;
+        private double localDeliveryCost;
+        private string worldRegion;
+        private string country;
+        private string region;
+        private int days;
+        private string dateTourStart;
+        private string dateTourEnd;
+        private string nextDateCheck = "9/9/9999";
+        private string hotelStars;
+        private string room;
+        private string meal;
+        private string included;
+        private string transport;
+
+
+
+        public Tour(XmlNode allData) : base(allData)
+        {
+            ParseOfferLine();
+            ParseBase();
+            ParseOther();
+        }
+
+
+        private void ParseOther()
+        {
+            foreach (XmlNode childNode in AllData.ChildNodes)
+            {
+                if (childNode.Name == "delivery")
+                    delivery = Boolean.Parse(childNode.InnerText);
+
+                if (childNode.Name == "local_delivery_cost")
+                    localDeliveryCost = Double.Parse(childNode.InnerText);
+
+                if (childNode.Name == "worldRegion")
+                    worldRegion = childNode.InnerText;
+
+                if (childNode.Name == " country")
+                    country = childNode.InnerText;
+
+                if (childNode.Name == "region")
+                    region = childNode.InnerText;
+
+                if (childNode.Name == "days")
+                    days = Int32.Parse(childNode.InnerText);
+
+                /*
+                 * Для блоков :if (!CheckDate(childNode.InnerText, nextDateCheck)) - if (CheckDate(childNode.InnerText, nextDateCheck))
+                 * Находит меньшую из дат и присваевает ее dateTourStart, большую -> dateTourEnd
+                 */
+                if (childNode.Name == "dataTour")
+                    if (!CheckDate(childNode.InnerText, nextDateCheck)) {
+                        dateTourStart = childNode.InnerText;
+                        nextDateCheck = childNode.InnerText;
+                    }
+
+                if (childNode.Name == "dataTour")
+                    if (CheckDate(childNode.InnerText, nextDateCheck))
+                        dateTourEnd = childNode.InnerText;
+
+                if (childNode.Name == "hotelStars")
+                    hotelStars = childNode.InnerText;
+
+                if (childNode.Name == "room")
+                    room = childNode.InnerText;
+
+                if (childNode.Name == "meal")
+                    meal = childNode.InnerText;
+
+                if (childNode.Name == "included")
+                    included = childNode.InnerText;
+
+                if (childNode.Name == "transport")
+                    transport = childNode.InnerText;
+            }
+
+        }
+
+        /*
+         *Возвращает true, если data1 > data2
+         *Проверка начинается с года, потом месяц, потом день
+         */
+        private bool CheckDate(string date1, string date2)
+        {
+
+            if (Int32.Parse(date1.Split("/")[2]) > Int32.Parse(date2.Split("/")[2]))
+                return true;
+
+            else if (Int32.Parse(date1.Split("/")[1]) > Int32.Parse(date2.Split("/")[1]))
+                return true;
+
+            else if (Int32.Parse(date1.Split("/")[0]) > Int32.Parse(date2.Split("/")[0]))
+                return true;
+            else
+                return false;
+        }
+    }
+
+    class EventTicket : OfferType
+    {
+
+        private bool delivery;
+        private double localDeliveryCost;
+        private string name;
+        private string place;
+        private string hallPlanLink;
+        private string hallPlan;
+        private string hallPart;
+        private string date;
+        private int isPremiere;
+        private int isKids;
+
+        public EventTicket(XmlNode allData) : base(allData)
+        {
+            ParseOfferLine();
+            ParseBase();
+            ParseOther();
+        }
+
+        private void ParseOther()
+        {
+            foreach (XmlNode childNode in AllData.ChildNodes)
+            {
+                if (childNode.Name == "delivery")
+                    delivery = Boolean.Parse(childNode.InnerText);
+
+                if (childNode.Name == "local_delivery_cost")
+                    localDeliveryCost = Double.Parse(childNode.InnerText);
+
+                if (childNode.Name == "name")
+                    name = childNode.InnerText;
+
+                if (childNode.Name == "place")
+                    place = childNode.InnerText;
+
+                if (childNode.Name == "hall")
+                    hallPlanLink = childNode.Attributes.GetNamedItem("plan").Value;
+
+                if (childNode.Name == "hall")
+                    hallPlan = childNode.InnerText;
+
+                if (childNode.Name == "hall_part")
+                    hallPart = childNode.InnerText;
+
+                if (childNode.Name == "date")
+                    date = childNode.InnerText;
+
+                if (childNode.Name == "is_premiere")
+                    isPremiere = Int32.Parse(childNode.InnerText);
+
+                if (childNode.Name == "isKids")
+                    isKids = Int32.Parse(childNode.InnerText);
+            }
+        }
+    }
+
+}
---
+++ TestTask/MainActivity.cs
@@ -0,0 +1,166 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using System.Net;
+using System.Collections.Generic;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Xml;
+using Newtonsoft.Json;
+
+
+
+namespace TestTask
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+
+        ArrayAdapter<string> adapter;
+        /*
+         * Ассоциативный массив Id -> offer.
+         * По Id можно найти любой оффер и вытащить необходимые данные(реализовав предваритьно геттер)
+         */
+        Dictionary<int, OfferType> IDstoOfferClasses = new Dictionary<int, OfferType>();
+        /*
+         * Список ID'шников
+         */
+        private List<string> IDs = new List<string>();
+
+        [Obsolete]
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            GetData();
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            ListView list = FindViewById<ListView>(Resource.Id.listView1);
+            list.SetAdapter(adapter);
+
+            list.ItemClick += List_ItemClick;
+        }
+
+        /*
+         * Получение данных
+         */
+        [System.Obsolete]
+        private async void GetData()
+        {
+            string sURL;
+            sURL = "http://partner.market.yandex.ru/pages/help/YML.xml";
+
+            WebRequest wrGETURL;
+            wrGETURL = WebRequest.Create(sURL);
+
+            Stream objStream;
+            objStream = wrGETURL.GetResponse().GetResponseStream();
+            XmlDocument doc = new XmlDocument();
+            doc.Load(objStream);
+
+            /*
+             * Парсим файл
+             */
+            await Task.Run(() => Parse(doc));
+
+        }
+
+
+
+        [System.Obsolete]
+        private void Parse(XmlDocument doc)
+        {
+            XmlNode offers = doc.GetElementsByTagName("offers")[0];
+            XmlNode offer;
+            foreach (XmlNode xNode in offers)
+            {
+                if (xNode.Name == "offer")
+                {
+                    offer = xNode;
+                    /*
+                     * Добовляем offer в нужный класс
+                     * Так же создаем новую пару ID - offer
+                     */
+                    if (offer.Attributes.GetNamedItem("type").Value == "vendor.model")
+                    {
+                        VenderModel newOffer = new VenderModel(offer);
+                          IDstoOfferClasses.Add(newOffer.ID, newOffer);
+                    }
+                    if (offer.Attributes.GetNamedItem("type").Value == "book")
+                    {
+                        Book newOffer = new Book(offer);
+                          IDstoOfferClasses.Add(newOffer.ID, newOffer);
+                    }
+                    if (offer.Attributes.GetNamedItem("type").Value == "audiobook")
+                    {
+                        AudioBook newOffer = new AudioBook(offer);
+                          IDstoOfferClasses.Add(newOffer.ID, newOffer);
+                    }
+                    if (offer.Attributes.GetNamedItem("type").Value == "artist.title")
+                    {
+                        ArtistTitle newOffer = new ArtistTitle(offer);
+                         IDstoOfferClasses.Add(newOffer.ID, newOffer);
+                    }
+                    if (offer.Attributes.GetNamedItem("type").Value == "tour")
+                    {
+                        Tour newOffer = new Tour(offer);
+                        IDstoOfferClasses.Add(newOffer.ID, newOffer);
+                    
+                    }
+                    if (offer.Attributes.GetNamedItem("type").Value == "event-ticket")
+                    {
+                        EventTicket newOffer = new EventTicket(offer);
+                        IDstoOfferClasses.Add(newOffer.ID, newOffer);
+                    }
+                }
+
+            }
+            /*
+             * Переходим в сетДата, в котором получаем список и устонавливаем значения адаптера
+             * Переходим из этого метода, т.к он выполняется асинхронно, а если выполнить SetData в GetData есть шанс не заполнить массив IDtoOffer
+             */
+            SetData();
+        }
+
+        /*
+         * Запоняем список Id'шников и устонавливаем настройки адаптера
+         */
+        [System.Obsolete]
+        private void SetData()
+        {
+            foreach (var ID in IDstoOfferClasses.Keys)
+            {
+                IDs.Add(ID.ToString());
+            }
+            adapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1, IDs);
+        }
+
+        
+        /*
+         * Обработчик нажатия на строку ListView
+         * Переключается на новое activity с TextView, куда и выводит объект offer
+         * Реализуем Ассоциативный массив
+         */
+        private void List_ItemClick(object sender, AdapterView.ItemClickEventArgs e)
+        {
+            var item = adapter.GetItem(e.Position);
+            SetContentView(Resource.Layout.activity_message);
+            TextView textView = FindViewById<TextView>(Resource.Id.textView1);
+            OfferType offer;
+            if (IDstoOfferClasses.TryGetValue(Int32.Parse(item), out offer))
+            {
+                textView.Text = JsonConvert.SerializeObject(offer);
+            }
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
